f6c91433f2b9393f198885b0155fba2beb282082
Gab.  Adekeye <gabteezy14@gmail.com>
2021-03-11 13:43:07 +0100

    set ssl permission

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 
 export default {
-  DATABASE_URL: process.env.DATABASE_URL,
   username: process.env.PRODUCTION_USERNAME,
   password: process.env.PRODUCTION_PASSWORD,
   database: process.env.PRODUCTION_DATABASE,
