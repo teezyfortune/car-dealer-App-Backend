0176d3315384d266ee062079ce325ee755c6e699
Gab.  Adekeye <gabteezy14@gmail.com>
2021-03-16 10:06:06 +0100

    update redis codition

--- app/db/setup/redis.js
+++ app/db/setup/redis.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable no-constant-condition */
 import redis from 'redis';
 import { promisifyAll } from 'bluebird';
 import config from '../../../config/env';
@@ -7,29 +9,31 @@ promisifyAll(redis);
 
 const { NODE_ENV } = config;
 
+let redisDB;
+if (NODE_ENV === 'development' || 'production') {
 // Creates an instance of a Redis client.
-const redisDB = redis.createClient({
-  port: config.REDIS_PORT,
-  host: config.REDIS_HOST
-});
-redisDB.AUTH(config.REDIS_PASSWORD);
-// Selects a different database while in the testing environment
-if (NODE_ENV === 'test') {
-  redisDB.select(3, async (err) => {
-    if (err) {
-      logger.error(`An Error occurred while spawning a 
-    new Redis database with the following message: ${err.message}`);
-      process.exit(1);
-    } else {
-      try {
-        await redisDB.flushdbAsync();
-      } catch (e) {
-        logger.error(`An Error occurred while removing test keys with the message: ${e.message}`);
-      }
-    }
+  redisDB = redis.createClient({
+    port: config.REDIS_PORT,
+    host: config.REDIS_HOST
   });
+  redisDB.AUTH(config.REDIS_PASSWORD);
+  // Selects a different database while in the testing environment
+  if (NODE_ENV === 'test') {
+    redisDB.select(3, async (err) => {
+      if (err) {
+        logger.error(`An Error occurred while spawning a 
+    new Redis database with the following message: ${err.message}`);
+        process.exit(1);
+      } else {
+        try {
+          await redisDB.flushdbAsync();
+        } catch (e) {
+          logger.error(`An Error occurred while removing test keys with the message: ${e.message}`);
+        }
+      }
+    });
+  }
 }
-
 // Spawns a new redis connection instance that holds
 // the same configuration as the client above with an option to change configurations.
 const cloneRedisDB = (options = {}) => redisDB.duplicate(options,);
